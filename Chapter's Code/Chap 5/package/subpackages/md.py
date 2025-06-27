355fb0d7a8c435369140c0ed8733d74f9f84d224
aryanrajyaguru22 <aryanrajyaguru22@outlook.com>
2025-06-27 12:54:50 +0530

    Udemy

--- Chapter's Code/Chap 5/package/subpackages/md.py
+++ Chapter's Code/Chap 5/package/subpackages/md.py
@@ -1,9 +1,3 @@
-def addition(a,b):
-    """Returns the sum of a and b."""
-    return a + b
-def subtraction(a,b):
-    """Returns the difference of a and b."""
-    return a - b
 def multiplication(a,b):
     """Returns the product of a and b."""
     return a * b
